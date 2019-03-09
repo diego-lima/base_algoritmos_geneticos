6ad9950d1e7662d64461e3b9a911642983975aa3
Diego Lima <ddddiegolima@gmail.com>
2019-03-09 15:45:51 -0300

    Terminado processo geral, falta testar

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from random import random
 
 
 class Cromossomo:
@@ -13,26 +15,102 @@ class Cromossomo:
         self.genes = genes
 
     @staticmethod
-    def gerar():
+    def gerar() -> 'Cromossomo':
         """Gera um cromossomo aleatoriamente"""
         raise NotImplementedError("Esse método deve ser definido pela classe herdeira")
 
     @staticmethod
-    def avaliar(cromossomo):
+    def avaliar(cromossomo: 'Cromossomo'):
         """Avalia a aptidão do cromossomo.
         Essa função deve ser sobrescrita em cada problema específico."""
-        cromossomo.gerar()
         raise NotImplementedError("Essa função deve ser implementada especificamete para seu problema")
 
     @staticmethod
-    def mutacionar(cromossomo):
+    def mutacionar(cromossomo: 'Cromossomo', chance_mutacao: float) -> 'Cromossomo':
         """Modifica aleatoriamente um gene"""
         raise NotImplementedError("Esse método deve ser definido pela classe herdeira")
 
     @staticmethod
-    def reproduzir(pai, mae):
+    def reproduzir(pai: 'Cromossomo', mae: 'Cromossomo'):
         """Combina os genes dos dois cromossoos para gera um novo"""
         raise NotImplementedError("Esse método deve ser definido pela classe herdeira")
 
     def __repr__(self):
         return self.genes
+
+
+"""ENUMERAÇÕES"""
+
+
+class Reproducoes(Enum):
+    """
+    Tipos de reprodução implementadas
+    """
+    # Crossover de um ponto)
+    CROSSOVER_1 = 2
+
+
+class Selecoes(Enum):
+    """
+    Tipos de seleção implementadas
+    """
+    ROLETA = 1
+
+
+class Objetivos(Enum):
+    MINIMIZAR = 1
+    MAXIMIZAR = 2
+
+
+"""FUNÇÕES"""
+
+
+def sortear_roleta(cromossomos: list, avaliadora, qtd: int, objetivo: Objetivos = Objetivos.MINIMIZAR):
+    """
+    Recebe uma lista de cromossomos. Recebe a função avaliadora, que retorna a aptidão de um cromossomo.
+    Recebe também a quantidade de cromossomos a serem selecionados.
+
+    Retorna uma lista com os cromossomos relecionados.
+
+    A chance de um cromossomo ser sorteado é proporcional ao seu score dentre todos os outros scores.
+
+    Se o objetivo for MINIMIZAR, as menores aptidões terão mais chances. Se não, as maiores que terão.
+
+    OBSERVAÇÃO: a soma das chances só é 100% se o objetivo for MAXIMIZAR. Se for minimizar, passa de 100%,
+    ou seja, o menor score pode ter uma chance de 75%, o segundo de 50%, o terceiro de 30%...
+    """
+
+    lista_de_fitness_scores = [avaliadora(x) for x in cromossomos]
+
+    """Definir a forma de comparação, dependendo do objetivo"""
+    if objetivo == Objetivos.MINIMIZAR:
+        comparar = lambda sorteado, score: sorteado <= score
+    else:
+        comparar = lambda sorteado, score: sorteado >= score
+
+    """Gerar as faixas de cada cromossomo"""
+    acumulador_faixa = 0
+    lista_de_faixas = []
+
+    for cromossomo_fitness in lista_de_fitness_scores:
+        acumulador_faixa += cromossomo_fitness
+        lista_de_faixas.append(acumulador_faixa)
+
+    if objetivo == Objetivos.MINIMIZAR:
+        # A linha abaixo é só pra prevenir que o último cromossomo tenha 0% de chance, no caso de minimizar objetivo.
+        lista_de_faixas[-1] += lista_de_fitness_scores[-1]
+
+    total = sum(lista_de_fitness_scores)
+
+    cromossomos_selecionados = []
+    while len(cromossomos_selecionados) < qtd:
+        # Gerar um número entre 0 e o total de pontos de score
+        numero_sorteado = total * random()
+
+        """Pegar o índice que corresponde à faixa do número sorteado"""
+        for indice, aptidao in enumerate(lista_de_faixas):
+            if comparar(numero_sorteado, aptidao):
+                cromossomos_selecionados.append(cromossomos[indice])
+                break
+
+    return cromossomos_selecionados
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from random import randrange as numero_aleatorio
 
 class CromossomoQuadratico(Cromossomo):
     """
-    Essa classe vai tratar do problema de encontrar o mínimo da função
-    y = x^2
+    Essa classe vai tratar do problema de encontrar o mínimo da função y = x^2
+    Os cromossomos são uma sequência de 7 bits, onde o primeiro bit é o bit de sinal
+    ou seja, os cromossomos representarão números inteiros entre -63 e +63
     """
 
     @staticmethod
     def gerar():
-        """Geramos sequência de bits correspondentes a um número aleatório entre -63 e 63"""
+        """Geramos sequência de bits correspondentes a um número aleatório entre -numero_maximo e +numero_maximo"""
         numero_maximo = 63
 
         numero = numero_aleatorio(-numero_maximo, numero_maximo+1, 1)
@@ -25,8 +26,12 @@ class CromossomoQuadratico(Cromossomo):
         return x**2
 
     @staticmethod
-    def mutacionar(cromossomo):
-        """Modifica aleatoriamente um gene do cromossomo"""
+    def mutacionar(cromossomo: 'CromossomoQuadratico', chance_mutacao: float = 0.03):
+        """Modifica aleatoriamente um gene do cromossomo. Essa mutação tem uma chance chance_mutacao de acontecer"""
+        if random() > chance_mutacao:
+            # Não modificar
+            return cromossomo
+
         indice_gene = numero_aleatorio(0, len(cromossomo.genes))
         novos_genes = list(cromossomo.genes)
         gene = novos_genes[indice_gene]
@@ -35,8 +40,30 @@ class CromossomoQuadratico(Cromossomo):
         return CromossomoQuadratico(''.join(novos_genes))
 
     @staticmethod
-    def reproduzir(pai, mae):
-        pass
+    def reproduzir(pai: 'CromossomoQuadratico', mae: 'CromossomoQuadratico'):
+        """Podem existir vários métodos de reprodução.
+        Cada método deve estar implementado em uma função.
+        Essa função "reproduzir" vai chamar o método "default".
+        """
+
+        return CromossomoQuadratico.reproduzir_crossover_1(pai, mae)
+
+    @staticmethod
+    def reproduzir_crossover_1(pai: 'CromossomoQuadratico', mae: 'CromossomoQuadratico'):
+        """Essa reprodução será crossover de um ponto, onde o ponto é aleatório.
+        Eu gero um cromossomo, só pra pegar o seu tamanho.
+        Depois, eu escolho um ponto aleatório no meio do cromossomo (ou seja, sem pegar o primeiro e último gene)
+        e faço o crossover, gerando dois filhos.
+        """
+        # Tamanho do cromossomo. Esses -2 e +1 abaixo são pra
+        # eu pegar entre o primeiro e último gene, sem incluí-los.
+        num_genes = len(CromossomoQuadratico.gerar().genes) - 2
+        cut_point = int(num_genes * random()) + 1
+
+        filho_1 = pai.genes[:cut_point] + mae.genes[cut_point:]
+        filho_2 = mae.genes[:cut_point] + pai.genes[cut_point:]
+
+        return [filho_1, filho_2]
 
 
 if __name__ == "__main__":
@@ -44,26 +71,58 @@ if __name__ == "__main__":
     CONFIGURAÇÕES
     """
     # Quantos indivíduos vamos gerar
-    TAM_POPULACAO = 10
-    # Qual classe se responsabilizará pelo manuseio dos cromossomos
-    classe_cromossomo = CromossomoQuadratico
+    TAM_POPULACAO = 12
     # Quantas gerações queremos ter
     NUM_GERACOES = 150
+    # Método de seleção
+    SELECAO = Selecoes.ROLETA
+    # A geração, quantos cromossomos passam pela seleção
+    QTD_SELECIONADOS = 6
+    # Método de reprodução
+    REPRODUCAO = Reproducoes.CROSSOVER_1
+    # Chance de mutação
+    CHANCE_MUTACAO = 0.03
+    OBJETIVO = Objetivos.MINIMIZAR
+    # Qual classe se responsabilizará pelo manuseio dos cromossomos
+    classe_cromossomo = CromossomoQuadratico
     # Vamos guardar um histórico de todas gerações
     historico = []
 
     """
-    TRIAGEM
+    TRIAGEM / PROCESSO
     """
+
+    """População inicial"""
+
     # Geramos TAM_POPULACAO cromossomos aleatoriamente
     cromossomos = [classe_cromossomo.gerar() for x in range(TAM_POPULACAO)]
 
-    # Vamos fazer NUM_GERACOES iterações
     for _ in range(NUM_GERACOES):
         # TODO historico
-        # Guardamos a geração atual no histórico
-        # Ordenamos pela aptidão: os de menor (melhor) score são os primeiros
-        cromossomos.sort(key=classe_cromossomo.avaliar)
 
-        # Aplicamos uma mutação aleatória em todos cromossomos
-        cromossomos = map(CromossomoQuadratico.mutacionar, cromossomos)
+        """Seleção"""
+
+        if SELECAO == Selecoes.ROLETA:
+            cromossomos_selecionados = sortear_roleta(cromossomos, classe_cromossomo.avaliar, QTD_SELECIONADOS)
+
+        else:
+            cromossomos_selecionados = []
+
+        """Reprodução"""
+
+        if REPRODUCAO == Reproducoes.CROSSOVER_1:
+            cromossomos_filhos = []
+            while len(cromossomos_filhos) < TAM_POPULACAO:
+                # Faço uma roleta pra pegar dois pais aleatórios para gerar os filhos
+                pais = sortear_roleta(cromossomos_selecionados, classe_cromossomo.avaliar, 2)
+                cromossomos_filhos.extend(classe_cromossomo.reproduzir_crossover_1(*pais))
+
+        else:
+            cromossomos_filhos = []
+
+        """Mutação"""
+
+        cromossomos = [classe_cromossomo.mutacionar(filho, CHANCE_MUTACAO) for filho in cromossomos_filhos]
+
+    for cromossomo in cromossomos:
+        print(cromossomo, " (%d)" % classe_cromossomo.avaliar(cromossomo))
